141fa242fe88264af28b021b40d9ed259ce2ba82
victor <victohs.almeida131@gmail.com>
2024-10-10 11:24:34 -0300

    correção dos bug no header e no footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 
-    <footer> <!-- footer das paginas -->
+    <div id="footer"> <!-- footer das paginas -->
   
         <div id="perguntas"> <!-- sessão de ajuda -->
             <h1 id="titulo">Duvidas Frequentes</h1>
@@ -14,42 +14,44 @@
         </div>
 
         <div id="logo"><!-- a logo que fica no footer -->
-        <img src="multimidia/Logo_Foo.png" alt="logo da agro malandrin"  >
+            <a href="#header">
+            <img src="multimidia/Logo_Home.png" alt="logo da agro malandrin">
+            </a>
         </div>
 
         <div id="linksfora">  <!-- os links de fora (para outros sites) --> 
             <h1 id="titulo">Contate-nos</h1>
-            <table>
-                <tr>
-                    <td > <a href="https://www.facebook.com/agromalandrin/" target="_blank" id="linkfoo"> <li id="facefoo"> FaceBook </li> </a> </td>
+            
+                <div id="cont">
+                     <a href="https://www.facebook.com/agromalandrin/" target="_blank" id="linkfoo"> <li id="facefoo"> FaceBook </li> </a> 
 
-                    <td> <a href="https://www.instagram.com/agromalandrin/" target="_blank" id="linkfoo"> <li id="instfoo"> Instagram </li> </a> </td>                    
-                </tr>
-                <tr>
-                    <td> <a href="https://wa.me/551938431393"  target="_blank" id="linkfoo"> <li id="whatfoo"> Whatsapp </li> </a> </td>
+                     <a href="https://www.instagram.com/agromalandrin/" target="_blank" id="linkfoo"> <li id="instfoo"> Instagram </li> </a>                   
+                </div>
+                
+                <div id="cont">
+                    <a href="https://wa.me/551938431393"  target="_blank" id="linkfoo"> <li id="whatfoo"> Whatsapp </li> </a> 
                     
-                    <td>
-                        <button onclick="copiarTexto()" id="Emailbotum"> <li id="Emailfoo"> Email </li></button>
+                    
+                    <button onclick="copiarTexto()" id="Emailbotum"> <li id="Emailfoo"> Email </li></button>
 
-                        <script>
-                        function copiarTexto() {
-                                var email = "agromalandrin@hotmail.com"; // Texto a ser copiado
-                                // Tenta usar o método clipboard para copiar
-                                navigator.clipboard.writeText(email)
-                                .then(() => {
-                                    alert("Email copiado");
-                                })
-                                .catch(err => { 
-                                    alert("Erro encontrado, Tente novamente mais tarde");   
-                                });
-                            }
-                        </script>
-                    </td>
+                    <script>
+                    function copiarTexto() {
+                            var email = "agromalandrin@hotmail.com"; // Texto a ser copiado
+                            // Tenta usar o método clipboard para copiar
+                            navigator.clipboard.writeText(email)
+                            .then(() => {
+                                alert("Email copiado");
+                            })
+                            .catch(err => { 
+                                alert("Erro encontrado, Tente novamente mais tarde");   
+                            });
+                        }
+                    </script>
+                    
                 </tr>
-            </table>
+            </div>
         </div>
-   
-    </footer>
+    </div>
 </body>
 </html>
 
